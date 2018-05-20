6192a54e6bfa2229609978262f61ee9cc7ba9faf
Giselle Serate <giselle.serate@gmail.com>
2018-05-19 22:16:29 -0700

    Change filename; discover that this does not help; also discover that I've been pushing executables angery reax

--- bash_alias.c
+++ bash_alias.c
@@ -6,7 +6,7 @@ int main()
 	// It's mad about NULL as an argument, so we're passing in an array with a single nullptr. 
 	char* nullA[1] = {NULL};
 	printf("before execve\n");
-	execve("./testscript", nullA, nullA);
+	execve("./.testscript", nullA, nullA);
 	printf("returned to main\n");
 	return 0;
 }

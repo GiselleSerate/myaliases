a55a7dd2487171b78746ddf11c9b1446ce19367b
Giselle Serate <giselle.serate@gmail.com>
2018-05-20 18:09:00 -0700

    Run ls from C file

--- bash_alias.c
+++ bash_alias.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <unistd.h> // For execve.
+#include <stdlib.h> // in example code, don't know what this is for.
 
-int main()
+int main(int c, char *v[], char **env)
 {
-	// It's mad about NULL as an argument, so we're passing in an array with a single nullptr. 
-	char* nullA[1] = {NULL};
+	
+	char *exePath = "/bin/ls";
+	char *myArray[] = {NULL, NULL, NULL, NULL};
+
+	myArray[0] = "hello";
+//	myArray[1] = v[2];
+//	myArray[2] = v[3];
 	printf("before execve\n");
-	execve("./.testscript", nullA, nullA);
+	execve(exePath, myArray, env);
+
+	// It's mad about NULL as an argument, so we're passing in an array with a single nullptr. 
+//	char* nullA[1] = {NULL};
+//	printf("before execve\n");
+//	execve("./.testscript", nullA, nullA);
 	printf("returned to main\n");
 	return 0;
 }
